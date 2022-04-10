b53bde071a7f9491b3e090a8503ff142ac6ef487
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-10 03:46:24 +0100

    init repo

---
+++ producer/src/main/java/org/vefi/exchange/streams/producer/VefiExchangeStreamsProducerApplication.java
@@ -0,0 +1,12 @@
+package org.vefi.exchange.streams.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VefiExchangeStreamsProducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VefiExchangeStreamsProducerApplication.class, args);
+  }
+}
---
+++ producer/src/test/java/org/vefi/exchange/streams/producer/VefiExchangeStreamsProducerApplicationTests.java
@@ -0,0 +1,11 @@
+package org.vefi.exchange.streams.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VefiExchangeStreamsProducerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
